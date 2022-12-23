d010b60b2f9f179ac8bf7e4967a21049babc4adb
joelc <jcaples@ltisolutions.com>
2022-12-23 12:01:14 -0600

    Field tests

--- craps-simulator.tests/BetTests.cs
+++ craps-simulator.tests/BetTests.cs
@@ -58,25 +58,24 @@ namespace craps_simulator.tests {
         }
 
         [Theory]
-        [InlineData(1, 1, true, false, 10)] // Win on 2 or 12
-        [InlineData(1, 2, true, false, 5)]  // Win on other than 2 or 12
-        public void FieldBetWin(short die1, short die2, bool expectedIsWinner, bool expectedIsLoser, decimal expectedPays) {
+        [InlineData(1, 1, true, false, 10)] // Win on 2
+        [InlineData(6, 6, true, false, 10)] // Win on 12
+        [InlineData(1, 2, true, false, 5)]  // Win on other than 3
+        [InlineData(1, 3, true, false, 5)]  // Win on other than 4
+        [InlineData(3, 6, true, false, 5)]  // Win on other than 9
+        [InlineData(4, 6, true, false, 5)]  // Win on other than 10
+        [InlineData(5, 6, true, false, 5)]  // Win on other than 11
+        [InlineData(1, 4, false, true, 0)] // Lose on 5
+        [InlineData(1, 5, false, true, 0)] // Lose on 6
+        [InlineData(2, 5, false, true, 0)] // Lose on 7
+        [InlineData(4, 4, false, true, 0)] // Lose on 8
+        public void Field(short die1, short die2, bool expectedIsWinner, bool expectedIsLoser, decimal expectedPays) {
             var field = new Field();
             field.PlaceBet(5);
             var result = field.Result(new Game() { }, new(die1, die2));
             Assert.Equal(expectedIsWinner, result.IsWinner);
             Assert.Equal(expectedIsLoser, result.IsLoser);
             Assert.Equal(expectedPays, result.Pays);
-        }
-
-        [Fact]
-        public void FieldBetLose() {
-            var field = new Field();
-            field.PlaceBet(5);
-            var result = field.Result(new Game() { }, new(1, 4));
-            Assert.False(result.IsWinner);
-            Assert.True(result.IsLoser);
-            Assert.Equal(0, result.Pays);
         }
 
         [Theory]
